8fa82f81ffdca476366135c494428786b4a8ccb4
janapunya <punyabratajana022@gmail.com>
2025-06-02 21:47:09 +0530

    update vite

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'build'
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+    allowedHosts: [
+      'punyabrata-jana.onrender.com',
+      'localhost',
+      '127.0.0.1'
+    ]
   }
 })
